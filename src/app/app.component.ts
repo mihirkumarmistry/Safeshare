7612c466e3719e3f28958659c2ad572df4e04e26
Mihirkumar Mistry <mihir.mistry308@gmail.com>
2024-10-28 20:09:31 -0400

    theme integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SharedModule } from './theme/shared/shared.module';
+// import { AppRoutingModule } from './app-routing.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SharedModule,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,47 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './theme/layouts/admin-layout/admin-layout.component';
+import { GuestComponent } from './theme/layouts/guest/guest.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: '',
+      component: AdminComponent,
+      children: [
+        {
+          path: '',
+          redirectTo: '/dashboard/default',
+          pathMatch: 'full'
+        },
+        {
+          path: 'dashboard/default',
+          loadComponent: () => import('./demo/default/dashboard/dashboard.component').then((c) => c.DefaultComponent)
+        },
+        {
+          path: 'typography',
+          loadComponent: () => import('./demo/ui-component/typography/typography.component')
+        },
+        {
+          path: 'color',
+          loadComponent: () => import('./demo/ui-component/ui-color/ui-color.component')
+        },
+        {
+          path: 'sample-page',
+          loadComponent: () => import('./demo/other/sample-page/sample-page.component')
+        }
+      ]
+    },
+    {
+      path: '',
+      component: GuestComponent,
+      children: [
+        {
+          path: 'login',
+          loadComponent: () => import('./demo/authentication/login/login.component')
+        },
+        {
+          path: 'register',
+          loadComponent: () => import('./demo/authentication/register/register.component')
+        }
+      ]
+    }
+  ];
